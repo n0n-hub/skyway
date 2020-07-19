8acfcb2c6343e65d0352d463d9aea5c8ab4366ab
n0n-hub <68460236+n0n-hub@users.noreply.github.com>
2020-07-19 20:44:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,42 @@
-'use strict'
-let localStream = null;
-let peer = null ;
-let existingCall = null;
-
-// カメラ映像取得
-navigator.mediaDevices.getUserMedia({video: true, audio: true})
-  .then( function (stream) {
-  // 成功時にvideo要素にカメラ映像をセットし、再生
-
-  $('#my-video').get(0).srcObject = stream;
-  localStream = stream;
-  }).catch(function(error){
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
-  });
-
-  peer = new Peer({
-    key: '23dd3897-440e-4c7a-9515-c39be8185baa',
+ const peer = new Peer({
+    key: '6126cc8e-5c0e-4fa4-82c9-8391682effbe',
     debug: 3
   });
 
-  peer.on('open', function(){
-    $('#my-id').text(peer.id);
-  });
-  peer.on('error', function(err){
-    alert(err.message);
-  });
-  peer.on('close', function(){
-  });
-  peer.on('disconnected',function(){
-  });
-  $('#make-call').submit(function(e){
-    e.preventDefault();
-    const call = peer.call($('#callto-id').val(), localStream);
-    setupCallEventHanders(call);
-  });
-  $('#end-call').click(function(){
-    existingCall.close();
-  });
-  peer.on('call',function(call){
-    call.answer(localStream);
-    setupCallEventHanders(call);
-  });
-  function setupCallEventHanders(call){
-    if (existingCall){
-        existingCall.close();
-          };
+peer.on('open', () => {
+    document.getElementById('my-id').textContent = peer.id;
+});
 
-    existingCall = call;
-    call.on('stream',function(stream){
-      addVideo(call.stream);
-      setupEndCallUI();
-      $('#their-id').text(call.remoteId);
-    });
-    call.on('close',function(){
-      removeVideo(call.remoteId);
-      setupMakeCallUI();
-    });
-  }
-  function addVideo(call,stream){
-    $('#their-video').get(0).srcObject = stream;
-  }
-  function removeVideo(peerId){
-    $('#their-video').get(0).srcObject = undefined;
-  }
-  function setupMakeCallUI(){
-    $('#make-call').show();
-    $('#end-call').hide();
-  }
+// 発信処理
+document.getElementById('make-call').onclick = () => {
+  const theirID = document.getElementById('their-id').value;
+  const mediaConnection = peer.call(theirID, localStream);
+  setEventListener(mediaConnection);
+};
 
-  function setupEndCallUI(){
-    $('#make-call').hide();
-    $('#end-call').show();
- }
- 
+// イベントリスナを設置する関数
+const setEventListener = mediaConnection => {
+  mediaConnection.on('stream', stream => {
+    // video要素にカメラ映像をセットして再生
+    const videoElm = document.getElementById('their-video')
+    videoElm.srcObject = stream;
+    videoElm.play();
+  });
+}
+
+// 発信処理
+document.getElementById('make-call').onclick = () => {
+  const theirID = document.getElementById('their-id').value;
+  const mediaConnection = peer.call(theirID, localStream);
+  setEventListener(mediaConnection);
+};
+
+// イベントリスナを設置する関数
+const setEventListener = mediaConnection => {
+  mediaConnection.on('stream', stream => {
+    // video要素にカメラ映像をセットして再生
+    const videoElm = document.getElementById('their-video')
+    videoElm.srcObject = stream;
+    videoElm.play();
+  });
+}
