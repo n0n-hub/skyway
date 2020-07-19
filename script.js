9606ee7d9b1287d1b9b772c358093bba69287099
n0n-hub <68460236+n0n-hub@users.noreply.github.com>
2020-07-19 20:59:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,96 @@
- const peer = new Peer({
-    key: '6126cc8e-5c0e-4fa4-82c9-8391682effbe',
-    debug: 3
-  });
-
-peer.on('open', () => {
-    document.getElementById('my-id').textContent = peer.id;
+// カメラ／マイクにアクセスするためのメソッドを取得しておく
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+ 
+var localStream;    // 自分の映像ストリームを保存しておく変数
+var connectedCall;  // 接続したコールを保存しておく変数
+ 
+// SkyWayのシグナリングサーバーへ接続する (APIキーを置き換える必要あり）
+var peer = new Peer({ key: '6126cc8e-5c0e-4fa4-82c9-8391682effbe', debug: 3});
+ 
+// シグナリングサーバへの接続が確立したときに、このopenイベントが呼ばれる
+peer.on('open', function(){
+    // 自分のIDを表示する
+    // - 自分のIDはpeerオブジェクトのidプロパティに存在する
+    // - 相手はこのIDを指定することで、通話を開始することができる
+    $('#my-id').text(peer.id);
 });
-
-// 発信処理
-document.getElementById('make-call').onclick = () => {
-  const theirID = document.getElementById('their-id').value;
-  const mediaConnection = peer.call(theirID, localStream);
-  setEventListener(mediaConnection);
-};
-
-// イベントリスナを設置する関数
-const setEventListener = mediaConnection => {
-  mediaConnection.on('stream', stream => {
-    // video要素にカメラ映像をセットして再生
-    const videoElm = document.getElementById('their-video')
-    videoElm.srcObject = stream;
-    videoElm.play();
-  });
-}
-
-// 発信処理
-document.getElementById('make-call').onclick = () => {
-  const theirID = document.getElementById('their-id').value;
-  const mediaConnection = peer.call(theirID, localStream);
-  setEventListener(mediaConnection);
-};
-
-// イベントリスナを設置する関数
-const setEventListener = mediaConnection => {
-  mediaConnection.on('stream', stream => {
-    // video要素にカメラ映像をセットして再生
-    const videoElm = document.getElementById('their-video')
-    videoElm.srcObject = stream;
-    videoElm.play();
-  });
-}
+ 
+// 相手からビデオ通話がかかってきた場合、このcallイベントが呼ばれる
+// - 渡されるcallオブジェクトを操作することで、ビデオ映像を送受信できる
+peer.on('call', function(call){
+  　
+    // 切断時に利用するため、コールオブジェクトを保存しておく
+    connectedCall = call;
+ 
+    // 相手のIDを表示する
+    // - 相手のIDはCallオブジェクトのpeerプロパティに存在する
+    $("#peer-id").text(call.peer);
+ 
+    // 自分の映像ストリームを相手に渡す
+    // - getUserMediaで取得したストリームオブジェクトを指定する
+    call.answer(localStream);
+ 
+    // 相手のストリームが渡された場合、このstreamイベントが呼ばれる
+    // - 渡されるstreamオブジェクトは相手の映像についてのストリームオブジェクト
+    call.on('stream', function(stream){
+ 
+        // 映像ストリームオブジェクトをURLに変換する
+        // - video要素に表示できる形にするため変換している
+        var url = URL.createObjectURL(stream);
+ 
+        // video要素のsrcに設定することで、映像を表示する
+        $('#peer-video').prop('src', url);
+    });
+});
+ 
+// DOM要素の構築が終わった場合に呼ばれるイベント
+// - DOM要素に結びつく設定はこの中で行なう
+$(function() {
+ 
+    // カメラ／マイクのストリームを取得する
+    // - 取得が完了したら、第二引数のFunctionが呼ばれる。呼び出し時の引数は自身の映像ストリーム
+    // - 取得に失敗した場合、第三引数のFunctionが呼ばれる
+    navigator.getUserMedia({audio: true, video: true}, function(stream){
+ 
+        // このストリームを通話がかかってき場合と、通話をかける場合に利用するため、保存しておく
+        localStream = stream;
+ 
+        // 映像ストリームオブジェクトをURLに変換する
+        // - video要素に表示できる形にするため変換している
+        var url = URL.createObjectURL(stream);
+ 
+        // video要素のsrcに設定することで、映像を表示する
+        $('#my-video').prop('src', url);
+ 
+    }, function() { alert("Error!"); });
+ 
+    // Start Callボタンクリック時の動作
+    $('#call-start').click(function(){
+ 
+        // 接続先のIDをフォームから取得する
+        var peer_id = $('#peer-id-input').val();
+ 
+        // 相手と通話を開始して、自分のストリームを渡す
+        var call = peer.call(peer_id, localStream);
+            
+        // 相手のストリームが渡された場合、このstreamイベントが呼ばれる
+        // - 渡されるstreamオブジェクトは相手の映像についてのストリームオブジェクト
+        call.on('stream', function(stream){
+            // 相手のIDを表示する
+            $("#peer-id").text(call.peer);
+ 
+            // 映像ストリームオブジェクトをURLに変換する
+            // - video要素に表示できる形にするため変換している
+            var url = URL.createObjectURL(stream);
+ 
+            // video要素のsrcに設定することで、映像を表示する
+            $('#peer-video').prop('src', url);
+        });
+    });
+ 
+    // End　Callボタンクリック時の動作
+    $('#call-end').click(function(){
+        // ビデオ通話を終了する
+        connectedCall.close();
+    });
+});
